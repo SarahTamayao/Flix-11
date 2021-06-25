a241824fb0d4921a608f30e9fde52c1ae9597f96
Dylan Yoshi Fernandez de Lara <dylanfdl@fb.com>
2021-06-25 16:29:21 -0500

    Pre-save before messing with background and gradients

--- Flix/View Controllers/DetailsPlusViewController.m
+++ Flix/View Controllers/DetailsPlusViewController.m
@@ -14,6 +14,8 @@
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet UILabel *synopsisLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *backgroundShape;
+@property (weak, nonatomic) IBOutlet UILabel *langLabel;
+@property (weak, nonatomic) IBOutlet UILabel *releaseDateLabel;
 
 
 @end
@@ -23,32 +25,43 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // rounded corners for backgroundShape
-    self.backgroundShape.clipsToBounds = TRUE;
-    self.backgroundShape.layer.cornerRadius = 20;
+    //self.backgroundShape.clipsToBounds = TRUE;
+    self.backgroundShape.layer.cornerRadius = 5;
     
     // Do any additional setup after loading the view.
     // To be used for both back and main poster
     NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
     
-    // main poster url
+    /*// main poster url
     NSString *posterURLString = self.movie[@"poster_path"];
     NSString *fullPosterURLString = [baseURLString stringByAppendingString:posterURLString];
     NSURL *posterURL = [NSURL URLWithString:fullPosterURLString];
-    [self.posterView setImageWithURL:posterURL];
+    [self.posterView setImageWithURL:posterURL];*/
     
     // back poster url
     NSString *backdropURLString = self.movie[@"backdrop_path"];
     NSString *fullbackdropURLString = [baseURLString stringByAppendingString:backdropURLString];
     NSURL *backposterURL = [NSURL URLWithString:fullbackdropURLString];
     [self.backdropView setImageWithURL:backposterURL];
+    //self.backdropView.layer
     
     // set title
     self.titleLabel.text = self.movie[@"title"];
+    [self.titleLabel setText:[self.titleLabel.text uppercaseString]];
     [self.titleLabel sizeToFit];
     
     // set synopsis
     self.synopsisLabel.text = self.movie[@"overview"];
     [self.synopsisLabel sizeToFit];
+    
+    // set release data
+    self.releaseDateLabel.text = self.movie[@"release_date"];
+    [self.releaseDateLabel sizeToFit];
+    
+    // set lang
+    self.langLabel.text = self.movie[@"original_language"];
+    [self.langLabel setText:[self.langLabel.text uppercaseString]];
+    [self.langLabel sizeToFit];
 }
 
 /*
